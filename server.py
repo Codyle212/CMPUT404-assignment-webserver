359b8f7ce258115e54e4ef1a91649f42f757b718
Shi Ning Le <shining@ualberta.ca>
2022-01-27 01:19:18 -0700

    fiexs

--- server.py
+++ server.py
@@ -43,7 +43,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
             # 405 if not GET
             self.request.sendall(
                 bytearray("HTTP/1.1 405 Method Not Allowed", 'utf-8'))
-        elif '..' in request_route:
+        elif '/..' in request_route:
             self.request.sendall(
                 bytearray("HTTP/1.1 404 Not Found\r\n", 'utf-8'))
         else:
